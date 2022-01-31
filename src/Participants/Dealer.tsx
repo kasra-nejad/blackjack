5dad00b03a1c332d611868a45ff559da2e30fad0
kasranejad <pfdclover@gmail.com>
2022-01-31 23:13:37 +0000

    players

---
+++ src/Participants/Dealer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {};
+
+const Dealer = (props: Props) => {
+  return <div></div>;
+};
+
+export default Dealer;
---
+++ src/Participants/Player.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {};
+
+const Player = (props: Props) => {
+  return <div></div>;
+};
+
+export default Player;
